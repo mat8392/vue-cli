3c080d7ba23c3857151e26ed487eb9549c1f877e
Nazrin Zulkifli <nazrin@hermo.my>
2019-03-29 09:27:28 +0800

    vue-cli is create with vue router

---
+++ src/components/Ninjas.vue
@@ -0,0 +1,53 @@
+<template>
+    <div id="ninjas">
+        <ul>
+            <li
+                v-for="(ninja, i) in ninjas"
+                v-on:click="ninja.show = !ninja.show"
+                :key="i"
+            >
+                <h2>{{ ninja.name }}</h2>
+                <h3 v-show="ninja.show">{{ ninja.speciality }}</h3>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ninjas',
+    props: {
+        ninjas: {
+            type: Array,
+            required: true
+        }
+    },
+    data() {
+        return {};
+    }
+};
+</script>
+
+<style scoped>
+#ninjas {
+    width: 100%;
+    max-width: 1200px;
+    margin: 40px auto;
+    padding: 0 20px;
+    box-sizing: border-box;
+}
+ul {
+    display: flex;
+    flex-wrap: wrap;
+    list-style-type: none;
+    padding: 0;
+}
+li {
+    flex-grow: 1;
+    flex-basis: 300px;
+    text-align: center;
+    padding: 30px;
+    border: 1px solid #222;
+    margin: 10px;
+}
+</style>
